02ea04295a678fed56440fc88f9c575a575e04af
Baek_do_eun <donna8780@naver.com>
2024-09-16 22:04:21 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.04 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String my_string, int n) {
+        String answer = "";
+        int l = my_string.length();
         
-        StringBuilder answer = new StringBuilder();
-        
-        int m = my_string.length() - n;  // 시작 위치를 계산
-        for (int i = m; i < my_string.length(); i++) {  // n만큼 뒤의 문자를 가져옴
-            answer.append(my_string.charAt(i));
-        }
-        return answer.toString();  // StringBuilder의 문자열을 반환
+        answer = my_string.substring(l-n);
+        return answer;
     }
 }
