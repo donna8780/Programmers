73d589dadaf176f2e1e24bde44d32c3176e3134e
Baek_do_eun <donna8780@naver.com>
2024-09-18 12:14:08 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.08 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        answer = my_string.startsWith(is_prefix) ? 1: 0;
+        return answer;
+    }
+}
