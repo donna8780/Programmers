7b3aeff60b31c748fc958ad141af7f334bef1f20
Baek_do_eun <donna8780@naver.com>
2024-09-22 15:09:44 +0900

    [level 1] Title: 햄버거 만들기, Time: 63.23 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,29 +2,23 @@ import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        
-        //for문 돌면서 스택에 넣기
-        //스택 사이즈가 4보다 크거나 같으면 맨위부터 1321이면
-        //맨 위에서부터 pop, answer을 1 올림
-        Stack<Integer> stack = new Stack<>();
-
-        for(int i = 0; i<ingredient.length; i++){
-       
-            stack.push(ingredient[i]);
-            if(stack.size() >= 4){
-                if(stack.get(stack.size()-1) == 1 && 
-                  stack.get(stack.size()-2) == 3 &&
-                  stack.get(stack.size()-3) == 2 &&
-                  stack.get(stack.size()-4) == 1 ) {
-                
-                 for(int j = 1; j<=4;j++){
-                     stack.pop();
-                     }
-                    answer +=1;
+        //1
+        Stack<Integer> list = new Stack<>();
+        for(int i : ingredient){
+            list.push(i);
+            if(list.size() >= 4){
+                if(list.get(list.size()-1) == 1 &&
+                    list.get(list.size()-2) == 3 &&
+                    list.get(list.size()-3)== 2 &&
+                    list.get(list.size()-4)==1){
+                    answer ++;
+                    for(int j = 0;j<4;j++){
+                        list.pop();
+                    }
                 }
-                }
+                    
+            }
         }
-      return answer;
+        return answer;
     }
-    
 }
