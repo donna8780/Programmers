1d25f21e9f5b74b83deb872debe57268996d4d4e
Baek_do_eun <donna8780@naver.com>
2025-05-14 13:29:52 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,14 @@
+function solution(age) {
+    //1. age 는 숫자로 들어옴.
+    //이걸 문자로 바꿔주는 로직 필요함
+    
+    const obj = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h', 8:'i', 9:'j'};
+    
+    let answer = ''; // 문자열로 반환되어야 함.
+    
+    for(let key of age.toString()){
+        answer += obj[key];
+    }
+    return answer;
+}
+//in은 존재유무를 확인할 때, of는 값을 꺼낼 때 사용한다.
