91c681a53782288f92114e127fdf007beacee32d
Baek_do_eun <donna8780@naver.com>
2025-05-29 16:18:20 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/README.md
+++ 프로그래머스/0/120819. 아이스 아메리카노/README.md
@@ -1,10 +1,10 @@
 # [level 0] 아이스 아메리카노 - 120819 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120819?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120819) 
 
 ### 성능 요약
 
-메모리: 81.4 MB, 시간: 0.02 ms
+메모리: 81.7 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 14일 22:41:57
+2025년 05월 29일 16:18:17
 
 ### 문제 설명
 
--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,10 +1,14 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
-        int gaesu = money / 5500;
-        int namuge = money % 5500;
-        answer[0] = gaesu;
-        answer[1] = namuge;
+        int result = money / 5500; // 잔의 개수
+        int remain = money - (5500*result); // 남은 돈
+        for (int i = 0; i<1; i++){
+            answer[i] = result;
+        }
+        for(int i = 1; i< 2; i++){
+            answer[i] = remain;
+        }
         return answer;
     }
 }
